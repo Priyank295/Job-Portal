6f95e9e407b9bf4806f4fc094e00cde56dd6e512
Priyank Vaghela <67854862+Priyank295@users.noreply.github.com>
2024-11-27 17:39:09 +0530

    remove unused Register component and related code

--- frontend/src/Recruiter/ApproveCandidate.jsx
+++ frontend/src/Recruiter/ApproveCandidate.jsx
@@ -169,7 +169,7 @@ const ApproveCandidate = () => {
   };
 
   return (
-    <div className="p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+    <div className=" p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
       {data.length === 0 ? (
         <div className="col-span-full text-center text-lg font-medium">
           No applications are there
